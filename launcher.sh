c99b7eb19c1e5fbbb4464c16662ff4be84268ce7
pjbooth <paul_booth_uk@hotmail.com>
2015-10-18 22:40:44 +0100

    removed launcher.sh

--- launcher.sh
+++
@@ -1,7 +0,0 @@
-echo 'Starting launcher'
-cd /home/pi/SD14-Hay-Steamer
-echo 'pulling git'
-sudo git pull origin
-sleep 10
-echo 'starting SD14Main.py'
-sudo python /home/pi/SD14-Hay-Steamer/SD14Main.py &
