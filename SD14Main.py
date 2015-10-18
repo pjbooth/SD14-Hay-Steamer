6e2262bd2b69ca861c33ed231804dcec5c13d038
pjbooth <paul_booth_uk@hotmail.com>
2015-10-18 21:12:13 +0100

    x

--- SD14Main.py
+++ SD14Main.py
@@ -69,7 +69,8 @@ def on_message(client, userdata, msg):
 
 def myCommandCallback(cmd):						# callback example from IOTF documentation
 	print("Command received: %s" % cmd.command)
-	print("Command received: %s" % cmd.command)
+	print("Data received: %s" % cmd.data)
+	print("Message received: %s" % cmd.data.message)
 
 	if cmd.command == "setInterval":
 		if 'interval' not in cmd.data:
