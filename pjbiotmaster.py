73ee0730118527caf35d6c636faae26dbb7b77e8
pjbooth <paul_booth_uk@hotmail.com>
2015-11-29 21:19:12 +0000

    x

--- pjbiotmaster.py
+++ pjbiotmaster.py
@@ -74,7 +74,7 @@ def reboot():
 
 
 def postField(s):			# a simple test to see if I can post to ThingSpeak
-	r = requests.post("https://api.thingspeak.com/update.json?api_key=Y5GJMXA8DG5GRESM", data={'field7': s})
+	r = requests.post("https://api.thingspeak.com/update.json?api_key=Y5GJMXA8DG5GRESM", data={'field7': 2})
 
 
 ###########  end of defs  ##################
